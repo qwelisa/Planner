d94651d0dd38665f552dc0ef4125c948b1ed8bc4
qwelisa <elisabiserni07@gmail.com>
2026-02-25 09:42:48 +0000

    import necessari

---
+++ app.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import pb from './pocketbase';
+import Task from './components/Task';
+import Calendar from './components/Calendar';
+import './App.css';
+
